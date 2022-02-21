230ddbf1445fb74bc65a0d733694a2d5242b6cbb
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-02-21 13:48:28 -0800

    Fix project structure
    - Removed local files from Git (should only be present locally)
    - Properly set up the Spigot plugin in Maven
    - Updated to 1.18 API
    - Removed erroneous "main.java" prefixes
    - Fixed several redundancy and NullPointerException warnings

--- src/main/java/net/fruchtlabor/composterplus/listeners/FermentingListener.java
+++ src/main/java/net/fruchtlabor/composterplus/listeners/FermentingListener.java
@@ -1,32 +1,28 @@
-package main.java.net.fruchtlabor.composterplus.listeners;
+package net.fruchtlabor.composterplus.listeners;
 
-import main.java.net.fruchtlabor.composterplus.ComposterPlus;
-import main.java.net.fruchtlabor.composterplus.Loot;
-import main.java.net.fruchtlabor.composterplus.SpecialCompost;
-import org.bukkit.block.data.BlockData;
+import net.fruchtlabor.composterplus.ComposterPlus;
+import net.fruchtlabor.composterplus.Loot;
+import net.fruchtlabor.composterplus.SpecialCompost;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Hopper;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Levelled;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 import java.util.List;
 import java.util.Objects;
-
-import org.bukkit.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_16_R2.entity.CraftExperienceOrb;
-import org.bukkit.entity.ExperienceOrb;
-import org.bukkit.block.Hopper;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.Location;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.event.EventHandler;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.block.data.Levelled;
-import org.bukkit.Material;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.event.Listener;
 
 public class FermentingListener implements Listener
 {
@@ -55,7 +51,7 @@ public class FermentingListener implements Listener
             }
             if (loot.getXp() > 0) {
                 final int orbs = loot.getXp();
-                ((ExperienceOrb)location.getWorld().spawn(location, ExperienceOrb.class)).setExperience(orbs);
+                Objects.requireNonNull(location.getWorld(), "World is null!").spawn(location, ExperienceOrb.class).setExperience(orbs);
             }
         }
         catch (Exception e) {
@@ -70,11 +66,13 @@ public class FermentingListener implements Listener
 
         try {
             final Material material = Material.matchMaterial(loot.getMat());
+            Objects.requireNonNull(material, "Material is null!");
             final ItemStack itemStack = new ItemStack(material, loot.getAmount());
-            location.getWorld().dropItemNaturally(location, itemStack);
+            Objects.requireNonNull(location.getWorld(), "World is null!")
+                    .dropItemNaturally(location, itemStack);
             if (loot.getXp() > 0) {
                 final int orbs = loot.getXp();
-                ((ExperienceOrb)location.getWorld().spawn(location, (Class)ExperienceOrb.class)).setExperience(orbs);
+                location.getWorld().spawn(location, ExperienceOrb.class).setExperience(orbs);
             }
         }
         catch (Exception e) {
@@ -100,7 +98,7 @@ public class FermentingListener implements Listener
     @EventHandler
     public void Fermenting(final PlayerInteractEvent event) {
 
-        if (event.getClickedBlock() != null && event.getClickedBlock().getType().equals((Object) Material.COMPOSTER)) {
+        if (event.getClickedBlock() != null && event.getClickedBlock().getType().equals(Material.COMPOSTER)) {
 
             final Block block = event.getClickedBlock();
             final BlockState state = block.getState();
@@ -113,16 +111,7 @@ public class FermentingListener implements Listener
 
                     if (compost.getMaterial().equalsIgnoreCase(event.getMaterial().name())) {
 
-                        if(lev.getLevel()+compost.getLevel()<lev.getMaximumLevel()){
-
-                            lev.setLevel(compost.getLevel() + lev.getLevel());
-
-                        }
-                        else {
-
-                            lev.setLevel(lev.getMaximumLevel());
-
-                        }
+                        lev.setLevel(Math.min(lev.getLevel() + compost.getLevel(), lev.getMaximumLevel()));
                         event.getPlayer().getInventory().getItemInMainHand().setAmount(event.getPlayer().getInventory().getItemInMainHand().getAmount() - 1);
                     }
 
@@ -156,15 +145,7 @@ public class FermentingListener implements Listener
                 for (SpecialCompost compost : ComposterPlus.sclist){
                     if(compost.getMaterial().equalsIgnoreCase(event.getItem().getType().name())){
 
-                        if(lev.getLevel()+compost.getLevel()<lev.getMaximumLevel()){
-
-                            lev.setLevel(compost.getLevel() + lev.getLevel());
-                        }
-                        else {
-
-                            lev.setLevel(lev.getMaximumLevel());
-
-                        }
+                        lev.setLevel(Math.min(lev.getLevel() + compost.getLevel(), lev.getMaximumLevel()));
 
                         event.setItem(new ItemStack(Material.AIR));
                     }
